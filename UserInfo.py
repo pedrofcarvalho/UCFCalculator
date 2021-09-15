2c1b768bcdb6dee40b0c2dac5ccad4c4f6034d2d
Pedro Fachetti Carvalho <pedro.fachetti@yahoo.com.br>
2021-09-15 01:04:05 -0400

    Classes Upgrade

--- UserInfo.py
+++ UserInfo.py
@@ -32,7 +32,11 @@ class NewClassesInfo:
 
     # constructor - all attributes are PRIVATE
     def __init__(self, course_name, course_grade, course_credit_hours):
+
+        # attributes that will be used as the string representation
         self.__course_name = course_name
+
+        # attributes that will be used as the ACTUAL DATA (MAKE A LIST FOR EACH IN USER() INSTEAD)
         self.__course_grade = course_grade
         self.__course_credit_hours = course_credit_hours
 
@@ -56,6 +60,7 @@ class NewClassesInfo:
     def get_credit_hours(self):
         return self.__course_credit_hours
 
+
     # setters
     def set_course_name(self, course_name):
         self.__course_name = course_name
@@ -67,52 +72,88 @@ class NewClassesInfo:
         self.__course_credit_hours = course_credit_hours
 
 
+class EmbedStringInfo:
+    # constructor - all attributes are PRIVATE
+    def __init__(self):
+        self.__class_names = str()
+        self.__grades = str()
+        self.__credit_hours = str()
+
+
 # wrapper class (will contain all info from a user)
 class User:
 
     # constructor - all attributes are PRIVATE
     def __init__(self, earned_credits, credits_taken):
+        # embed-used string attributes
+        self.__names_str = str()
+        self.__grades_str = str()
+        self.__hours_str = str()
 
         # can only create a User() with current info
         self.__current_info = OldClassesInfo(earned_credits, credits_taken)
 
-        # List will be empty AT FIRST
-        self.__new_classes_list = list[NewClassesInfo]
+        # list will be empty AT FIRST
+        self.__new_classes_list = []
 
         # size of list
         self.__class_list_size = int()
 
         # Reference to the last embed (for deletion purposes)
-        self.__last_embed_id = int()
+        self.__last_message_id = int()
+
+        # last created embed
+        self.__last_embed = None
 
         # Discord user that is the owner of the data
         self.__discord_user = str()
 
+
     # getters
-    async def get_current_info(self):
+    def get_current_info(self):
         return self.__current_info
 
-    async def get_class_list(self):
+    def get_classes(self):
         return self.__new_classes_list
 
-    async def get_list_size(self):
+    def get_class(self, index: int):
+        return self.__new_classes_list[index]
+
+    def get_list_size(self):
         return self.__class_list_size
 
-    async def get_last_embed(self):
-        return self.__last_embed_id
+    def get_last_embed(self):
+        return self.__last_embed
 
-    async def get_discord_user(self):
+    def get_last_message(self):
+        return self.__last_message_id
+
+    def get_discord_user(self):
         return self.__discord_user
 
+    def get_name_str(self):
+        return self.__names_str
+
+    def get_grades_str(self):
+        return self.__grades_str
+
+    def get_hours_str(self):
+        return self.__hours_str
+
+
     # setters
-    def set_user_embed(self, new_embed_id):
-        self.__last_embed_id = new_embed_id
+    def set_user_embed(self, new_embed):
+        self.__last_embed = new_embed
+
+    def set_last_message(self, last_id):
+        self.__last_message_id = last_id
 
     def set_discord_user(self, discord_user):
         self.__discord_user = discord_user
 
+
     # creates and add a new class to the User() list of new classes
-    async def add_class(self, course_name, course_grade, course_credit_hours: int):
+    def add_class(self, course_name, course_grade, course_credit_hours: int):
 
         # create and add new class to the list
         self.__new_classes_list.append(NewClassesInfo(course_name, course_grade, course_credit_hours))
@@ -121,13 +162,22 @@ class User:
         self.__class_list_size += 1
 
 
+    def concatenate_name_str(self):
+        last_element = self.__new_classes_list[self.__class_list_size - 1]
+        self.__names_str += last_element.get_course_name() + '\n'
+
+    def concatenate_grades_str(self):
+        last_element = self.__new_classes_list[self.__class_list_size - 1]
+        self.__grades_str += last_element.get_course_grade() + '\n'
+
+    def concatenate_hours_str(self):
+        last_element = self.__new_classes_list[self.__class_list_size - 1]
+        self.__hours_str += str(last_element.get_credit_hours()) + '\n'
+
 
     # mainly for debugging purposes
     def print_User(self):
-        print(f'current_info: {self.get_current_info()}')
-        print(f'class_list: {self.get_class_list()}')
-        print(f'last_embed: {self.get_last_embed()}')
-        print(f'discord_user: {self.get_discord_user()}')
+        print(f'[{self.__names_str}, {self.__grades_str}, {self.__hours_str}]')
 
 
 
@@ -142,7 +192,9 @@ class User:
 
 
 # not too important - FOR NOW
+# I NEED A HASH TABLE FOR USERS !!!!!!!!!!! (will deal with first users trying to use "add)
 class Database:
 
     def __init__(self):
-        self.__list_of_users = User()
+        self.__list_of_users = list[User] # CHANGE THIS TO HASH TABLE AFTERWARDS!!!!
+
